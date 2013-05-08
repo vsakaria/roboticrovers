b2762288bc9e8b46905603fd542c2bb5406fe638
Vishal Sakaria <vsakaria@gmail.com>
2013-05-08 14:57:55 +0100

    rover object created and rover can chane direction

--- js/run.js
+++ js/run.js
@@ -1,1 +1,33 @@
 
+var validDirection = ["N", "S", "E", "W"]
+
+createPlateau(0,0,5,5);
+
+createRover(1,2,"N");
+
+function createPlateau(x,y,a,b){
+  var bottomX = x;
+  var bottomY = y;
+  var topA = a;
+  var topB = b;
+
+  console.log("The Plateau" + " " + bottomX + " "  + bottomY + " " + topA + " " + topB)
+}
+
+function createRover(x,y,d){
+  var x = x;
+  var y = y;
+  var direction;
+
+  //Valiade the direction input
+	if ( $.inArray(d, validDirection) == -1)
+		console.log("Not a valid direction")
+	else
+		direction = d;
+		console.log("Lets start exploring with" + " " + x + " " + y + " " + direction )
+
+}
+
+
+
+
